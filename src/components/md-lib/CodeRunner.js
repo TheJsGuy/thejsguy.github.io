97b28d56c0e0f42ef7f2e09bc298b233077a14e1
Saini, Aman Kumar <amankumarsaini28@gmail.com>
2022-02-07 15:38:57 +0530

    added runtime variables

--- src/components/md-lib/CodeRunner.js
+++ src/components/md-lib/CodeRunner.js
@@ -1,8 +1,11 @@
 import React, { useCallback, useState } from "react";
 
-const codeWrapper = (code) => `function main() {${code}}`;
+const codeWrapper = (code) => `/*
+** @param context {{ log: (...string): void; inputs: any;}}
+*/
+function main(context) {${code}}`;
 
-const run = (code) => {
+const run = (code, inputs) => {
   let logs = "";
   const log = (...messages) =>
   (logs += (messages.reduce(
@@ -11,7 +14,7 @@ const run = (code) => {
   )) + '\n');
   try {
     const execute = Function("context", code);
-    const execution = execute({ log });
+    const execution = execute({ log, inputs });
     return `${logs}${execution || ''}`;
   } catch (error) {
     return `${logs}${error || ''}`;
@@ -19,18 +22,44 @@ const run = (code) => {
 
 };
 
-export const CodeRunner = ({ code }) => {
+export const CodeRunner = ({ code, inputs = {} }) => {
   const [output, setOutput] = useState();
+  const [input, setInput] = useState(JSON.stringify(inputs) || {});
+  const [inputHasError, setInputHasError] = useState(null);
+  const [parsedInput, setParsedInput] = useState(inputs);
+
+  const onInputChange = useCallback((event) => {
+    setInput(event.target.value);
+  }, [input]);
+
+  const onInputBlur = useCallback(() => {
+    try {
+      setParsedInput(JSON.parse(input));
+      setInputHasError(null);
+    } catch (error) {
+      setInputHasError(error);
+    }
+
+  }, [input, parsedInput, inputHasError]);
+
   const evaluate = useCallback(() => {
-    setOutput(run(code));
-  }, [code]);
+    setOutput(run(code, parsedInput));
+  }, [code, parsedInput]);
   return (
     <div>
-      <div>
-        
+      <div className="flex-col">
+        <label className="flex font-semibold">Inputs</label>
+        <textarea
+          onChange={onInputChange}
+          onBlur={onInputBlur}
+          className={`flex w-10/12 mt-2 border-2 border-slate-700 rounded border-solid outline-0 focus:outline-2 focus:outline-slate-500 ${inputHasError && 'border-rose-700 focus:outline-rose-500'}`}
+          value={input}
+        >
+        </textarea>
+        {inputHasError && <span className="text-rose-700">Invalid JSON</span>}
       </div>
       <div className="mt-5">
-        <label>Code <button
+        <label className="font-semibold">Code <button
           className="bg-slate-600 hover:bg-slate-500 px-2 py-1 text-white rounded cursor-pointer"
           onClick={evaluate}
         >
