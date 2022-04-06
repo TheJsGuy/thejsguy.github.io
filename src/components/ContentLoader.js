e9b278e8a6a82b43a30dbe653ba052fa399f110b
Aman Kumar Saini <amankumarsaini28@gmail.com>
2022-04-06 11:26:19 +0530

    added swiper code

--- src/components/ContentLoader.js
+++ src/components/ContentLoader.js
@@ -1,43 +1,55 @@
-import React, { useEffect } from 'react';
-import { Route, Routes, useNavigate } from 'react-router';
-import { useAppConfig } from '../context/app-config-context';
+import React, { useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router";
+import { useAppConfig } from "../context/app-config-context";
 
-import AboutMe from '../content/about-me.mdx';
-import ApplicationSetup from '../content/react/application-setup.mdx';
-import Test from '../content/problem-solving/test.mdx';
-import GenerateParentheses from '../content/problem-solving/generate-parentheses.mdx';
+import AboutMe from "../content/about-me.mdx";
+import ApplicationSetup from "../content/react/application-setup.mdx";
+import Test from "../content/problem-solving/test.mdx";
+import GenerateParentheses from "../content/problem-solving/generate-parentheses.mdx";
+import SwiperLib from "../content/libraries/thejsguy-swiper.mdx";
 
 const contentFactory = {
-    'about-me': AboutMe,
-    'application-setup': ApplicationSetup,
-    test: Test,
-    'generate-parentheses': GenerateParentheses
+  "about-me": AboutMe,
+  "application-setup": ApplicationSetup,
+  test: Test,
+  "generate-parentheses": GenerateParentheses,
+  'thejsguy-swiper': SwiperLib
 };
 
 const Default = () => {
-    let navigate = useNavigate();
-    useEffect(() => {
-        navigate('about-me')
-    })
-    return <br />;
-}
+  let navigate = useNavigate();
+  useEffect(() => {
+    navigate("about-me");
+  });
+  return <br />;
+};
 
 export const ContentLoader = () => {
-    const { nav: { links } } = useAppConfig();
+  const {
+    nav: { links },
+  } = useAppConfig();
 
-    return (
-        <Routes>
-            {links.map(link => {
-                if (!link.children) {
-                    const Component = contentFactory[link.key];
-                    return <Route key={link.key} path={link.key} element={<Component />} />
-                }
-                return link.children.map(subLink => {
-                    const Component = contentFactory[subLink.key];
-                    return <Route key={subLink.key} path={`${link.key}/${subLink.key}`} element={<Component />} />
-                });
-            })}
-            <Route path="*" element={<Default />} />
-        </Routes>
-    )
-}
+  return (
+    <Routes>
+      {links.map((link) => {
+        if (!link.children) {
+          const Component = contentFactory[link.key];
+          return (
+            <Route key={link.key} path={link.key} element={<Component />} />
+          );
+        }
+        return link.children.map((subLink) => {
+          const Component = contentFactory[subLink.key];
+          return (
+            <Route
+              key={subLink.key}
+              path={`${link.key}/${subLink.key}`}
+              element={<Component />}
+            />
+          );
+        });
+      })}
+      <Route path="*" element={<Default />} />
+    </Routes>
+  );
+};
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,42 +1,71 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { theme } from '../config/theme';
-import { useAppConfig } from '../context/app-config-context';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useAppConfig } from "../context/app-config-context";
 
-const NavItem = ({ link, noLink, children, route, className }) => (
-    noLink
-        ? <li><a className={`flex w-full px-4 py-2 uppercase text-sm mt-4 ${className || ''}`}>{link.label}</a>{children}</li>
-        : <li><NavLink to={route} className={({isActive}) => `flex w-full px-4 py-2 uppercase hover:bg-zinc-400 ${className || ''} ${isActive ? 'bg-zinc-400' : ''}`}>{link.label}</NavLink>{children}</li>
-
-);
+const NavItem = ({ link, noLink, children, route, className }) =>
+  noLink ? (
+    <li>
+      <a
+        className={`flex w-full px-4 py-2 uppercase text-sm mt-4 ${
+          className || ""
+        }`}
+      >
+        {link.label}
+      </a>
+      {children}
+    </li>
+  ) : (
+    <li>
+      <NavLink
+        to={route}
+        className={({ isActive }) =>
+          `flex w-full px-4 py-2 uppercase hover:bg-zinc-400 ${
+            className || ""
+          } ${isActive ? "bg-zinc-400" : ""}`
+        }
+      >
+        {link.label}
+      </NavLink>
+      {children}
+    </li>
+  );
 
 export const Navigation = () => {
-    const { nav: { links = [] } } = useAppConfig();
-    return (
-        <>
-            <div className="flex py-8 px-4">
-                <h1 className="text-2xl font-mono">Hello Github</h1>
-            </div>
-            <div className="flex flex-col">
-                <div className="flex flex-col">
-                    <ul className="list-none  w-full mt-2">
-                        {links.map(link => {
-                            if (!link.children) {
-                                return <NavItem key={link.key} link={link} route={link.key} />
-                            }
-                            return (
-                                <React.Fragment key={link.key}>
-                                    <NavItem link={link} noLink>
-                                        <ul className="list-disc w-full">
-                                            {link.children.map(subLink => <NavItem key={subLink.key} link={subLink} route={`${link.key}/${subLink.key}`} className="pl-7" />)}
-                                        </ul>
-                                    </NavItem>
-                                </React.Fragment>
-                            )
-                        })}
+  const {
+    nav: { links = [] },
+  } = useAppConfig();
+  return (
+    <>
+      <div className="flex py-8 px-4">
+        <h1 className="text-2xl font-mono">Hello Github</h1>
+      </div>
+      <div className="flex flex-col">
+        <div className="flex flex-col">
+          <ul className="list-none  w-full mt-2">
+            {links.map((link) => {
+              if (!link.children) {
+                return <NavItem key={link.key} link={link} route={link.key} />;
+              }
+              return (
+                <React.Fragment key={link.key}>
+                  <NavItem link={link} noLink>
+                    <ul className="list-disc w-full">
+                      {link.children.map((subLink) => (
+                        <NavItem
+                          key={subLink.key}
+                          link={subLink}
+                          route={`${link.key}/${subLink.key}`}
+                          className="pl-7"
+                        />
+                      ))}
                     </ul>
-                </div>
-            </div>
-        </>
-    );
-}
+                  </NavItem>
+                </React.Fragment>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
